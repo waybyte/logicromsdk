9fa327a25f25ccb6bedf0afa7d06284f06e8e084
Ajay Bhargav <contact@rickeyworld.info>
2020-05-29 21:21:51 +0530

    docs: Specify index.rst as master doc
    Signed-off-by: Ajay Bhargav <contact@rickeyworld.info>

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ copyright = '2020, Ajay Bhargav'
 author = 'Ajay Bhargav'
 
 # -- General configuration ---------------------------------------------------
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom

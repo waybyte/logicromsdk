a3b681e89ae51e31569dfbac3024880e394d6f62
Ajay Bhargav <contact@rickeyworld.info>
2021-08-04 19:53:23 +0530

    hw:i2c: multiport support added for future use
    Signed-off-by: Ajay Bhargav <contact@rickeyworld.info>

--- include/hw/i2c.h
+++ include/hw/i2c.h
@@ -6,6 +6,8 @@
 #ifndef INCLUDE_HW_I2C_H_
 #define INCLUDE_HW_I2C_H_
 
+#include <plat/def_i2c.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,42 +15,47 @@ extern "C" {
 /**
  * Initialize I2C hardware and setup pin-mux.
  * Please check hardware guide for I2C pin details.
+ * @param port			[in] I2C Port (@ref i2cport_e)
  * @param speed_khz		[in] I2C Speed in Khz (must be < 400)
  * @return				0 on success, negative value on error
  */
-int i2c_hw_init(int speed_khz);
+int i2c_hw_init(int port, int speed_khz);
 
 /**
  * Set I2C master clock speed
+ * @param port			[in] I2C Port (@ref i2cport_e)
  * @param speed_khz		[in] I2C Speed in Khz (must be < 400)
  * @return				0 on success, negative value on error
  */
-int i2c_hw_setspeed(int speed_khz);
+int i2c_hw_setspeed(int port, int speed_khz);
 
 /**
  * Master send data over I2C slave device
  * Hardware FIFO only support upto 8 bytes at one time for transfer
+ * @param port			[in] I2C Port (@ref i2cport_e)
  * @param address		[in] 7-bit Slave address
  * @param buffer		[in] Pointer to buffer containing data
  * @param len			[in] Length of data to send (must be less than or equal to 8)
  * @return				Length of bytes written on success, negative value on error
  */
-int i2c_hw_write(unsigned char address, unsigned char *buffer, int len);
+int i2c_hw_write(int port, unsigned char address, const unsigned char *buffer, int len);
 
 /**
  * Master read data from I2C slave device
+ * @param port			[in] I2C Port (@ref i2cport_e)
  * @param address		[in] 7-bit slave address
  * @param buffer		[out] Pointer to buffer to store data
  * @param len			[in] No. of bytes to read
  * @return				Length of data read from slave on success, negative value on error
  */
-int i2c_hw_read(unsigned char address, unsigned char *buffer, int len);
+int i2c_hw_read(int port, unsigned char address, unsigned char *buffer, int len);
 
 /**
  * Write and Read operation with repeated start.
  * Operation is performed in two transactions separated by repeated start, Each
  * transaction is limited to max fifo length of 8 bytes.
  *
+ * @param port			[in] I2C Port (@ref i2cport_e)
  * @param address		[in] 7-bit slave address
  * @param wrbuf			[in] Pointer to buffer for write operation
  * @param wrlen			[in] Length of data in @a wrbuf
@@ -56,13 +63,14 @@ int i2c_hw_read(unsigned char address, unsigned char *buffer, int len);
  * @param rdlen			[in] No. of bytes to read
  * @return				Length of data read from slave on success, negative value on error
  */
-int i2c_hw_writeread(unsigned char address, unsigned char *wrbuf, int wrlen, unsigned char *rdbuf, int rdlen);
+int i2c_hw_writeread(int port, unsigned char address, const unsigned char *wrbuf, int wrlen, unsigned char *rdbuf, int rdlen);
 
 /**
  * De-Init I2C bus and release GPIO
+ * @param port			[in] I2C Port (@ref i2cport_e)
  * @return				0 on success, negative value on error
  */
-int i2c_hw_free(void);
+int i2c_hw_free(int port);
 
 #ifdef __cplusplus
 }
---
+++ include/plat/def_i2c.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_PLAT_DEF_I2C_H_
+#define INCLUDE_PLAT_DEF_I2C_H_
+
+/**
+ * I2C port list
+ */
+enum i2cport_e {
+	I2C_PORT_0, /**< I2C Port 0 */
+};
+
+#endif /* INCLUDE_PLAT_DEF_I2C_H_ */

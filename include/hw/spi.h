efee5d94685e9653ab6f8868a3557e0b181c686c
Ajay Bhargav <contact@rickeyworld.info>
2021-08-04 19:53:56 +0530

    hw:spi: multiport support added for future use
    Signed-off-by: Ajay Bhargav <contact@rickeyworld.info>

--- include/hw/spi.h
+++ include/hw/spi.h
@@ -6,10 +6,7 @@
 #ifndef INCLUDE_HW_SPI_H_
 #define INCLUDE_HW_SPI_H_
 
-/**
- * Max supported Hardware SPI speed (default 10MHz)
- */
-#define SPI_MAX_SPEED	32000000U
+#include <plat/def_spi.h>
 
 /**
  * SPI Modes to configure clock polarity and clock phase.
@@ -26,21 +23,9 @@ enum spi_mode_e {
 /**
  * SPI byte order to configure shit in/out order of data byte.
  */
-enum spi_byteorder_e {
-	SPI_BYTE_MSB_FIRST,/**< MSB sent first (default) */
-	SPI_BYTE_LSB_FIRST,/**< LSB sent first */
-};
-
-/**
- * Chip select mode configures how chip select line is de-asserted during a
- * transaction. It only used when hardware chip select is used during
- * initialization. See @ref spi_hw_init
- */
-enum spi_csmode_e {
-	SPI_CSMODE_TRANSACTION,/**< CS line de-asserted between each transaction.
-								This is default behavior of CS line. */
-	SPI_CSMODE_BYTE,       /**< CS line de-asserted after each byte in a
-								transaction */
+enum spi_bitorder_e {
+	SPI_MSB_FIRST,/**< MSB sent first (default) */
+	SPI_LSB_FIRST,/**< LSB sent first */
 };
 
 /**
@@ -57,44 +42,52 @@ extern "C" {
 
 /**
  * Initialize SPI Hardware and setup pinmux
+ * @param port				[in] SPI Port Number (@ref spiport_e)
  * @param hardware_cs		[in] Set it TRUE, if application hardware chip
  * 								 select is used and hardware controls chip
  * 								 select status, FALSE otherwise
+ * @param clock				[in] SPI Clock speed in Hz
+ * @param mode				[in] SPI Mode (@ref spi_mode_e)
+ * @param cs_pol			[in] Chip select Polarity (@ref spi_cspol_e)
  * @return					0 on success, negative on error
  */
-int spi_hw_init(int hardware_cs);
+int spi_hw_init(int port, int hardware_cs, int clock, int mode, int cs_pol);
 
 /**
  * Set/Change SPI clock speed. Default speed is @ref SPI_MAX_SPEED
+ * @param port				[in] SPI Port Number (@ref spiport_e)
  * @param clock_hz			[in] Clock speed in Hz
  * @return					0 on success, negative value on error
  */
-int spi_hw_setclock(unsigned long clock_hz);
+int spi_hw_setclock(int port, unsigned long clock_hz);
 
 /**
  * Configure SPI clock mode
+ * @param port				[in] SPI Port Number (@ref spiport_e)
  * @param mode				[in] SPI mode, see @ref spi_mode_e
  * @return					0 on success, negative value on error
  */
-int spi_hw_setmode(int mode);
+int spi_hw_setmode(int port, int mode);
 
 /**
- * Configure SPI byte shift order
- * @param byte_order		[in] SPI byte order, see @ref spi_byteorder_e
+ * Configure SPI bit shift order
+ * @param port				[in] SPI Port Number (@ref spiport_e)
+ * @param bit_order			[in] SPI bit shit order, see @ref spi_bitorder_e
  * @return					0 on success, negative value on error
  */
-int spi_hw_setbyteorder(int byte_order);
+int spi_hw_setbitorder(int port, int bit_order);
 
 /**
  * Configure chip-select line. Only needed when hardware_cs is used
- * @param cs_mode			[in] Chip select mode, see @ref spi_csmode_e
+ * @param port				[in] SPI Port Number (@ref spiport_e)
  * @param cs_pol			[in] Chip select polarity, see @ref spi_cspol_e
  * @return					0 on success, negative value on error
  */
-int spi_hw_setcsmode(int cs_mode, int cs_pol);
+int spi_hw_setcsmode(int port, int cs_pol);
 
 /**
  * Perform SPI transaction
+ * @param port				[in] SPI Port Number (@ref spiport_e)
  * @param wrbuf				[in] Pointer to data buffer to write, can be NULL
  * 								 if only read operation to be performed
  * @param rdbuf				[out] Pointer to buffer to store data, can be NULL
@@ -102,13 +95,14 @@ int spi_hw_setcsmode(int cs_mode, int cs_pol);
  * @param length			[in] Length of data to read/write
  * @return					Length of data read/write on success, negative on error
  */
-int spi_hw_transfer(const unsigned char *wrbuf, unsigned char *rdbuf, int length);
+int spi_hw_transfer(int port, const unsigned char *wrbuf, unsigned char *rdbuf, int length);
 
 /**
  * Release SPI hardware
+ * @param port				[in] SPI Port Number (@ref spiport_e)
  * @return					0 on success, negative on error
  */
-int spi_hw_free(void);
+int spi_hw_free(int port);
 
 #ifdef __cplusplus
 }
---
+++ include/plat/def_spi.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_PLAT_DEF_SPI_H_
+#define INCLUDE_PLAT_DEF_SPI_H_
+
+/**
+ * SPI port list
+ */
+enum spiport_e {
+	SPI_PORT_0, /**< SPI Port 0 */
+};
+
+#endif /* INCLUDE_PLAT_DEF_SPI_H_ */

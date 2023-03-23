a6f58b6b7f14e8b4b2d9207b144b27738d304ad8
Ajay Bhargav <ajay.bhargav@waybyte.in>
2023-03-23 15:37:46 +0530

    include: add sha256 hash API
    Signed-off-by: Ajay Bhargav <ajay.bhargav@waybyte.in>

--- include/crypto/sha.h
+++ include/crypto/sha.h
@@ -18,6 +18,15 @@ extern "C" {
  */
 void sha1(const unsigned char *input, int ilen, unsigned char output[20]);
 
+/**
+ * @brief Calculate sha256 hash
+ * 
+ * @param input [in] Input data buffer
+ * @param ilen [in] Input data length
+ * @param output [out] Output buffer to store hash
+ */
+void sha256(const unsigned char *input, int ilen, unsigned char output[32]);
+
 #ifdef __cplusplus
 }
 #endif
